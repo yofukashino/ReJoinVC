c4558a4570036966faa6f4d92cdd7edfc54a6fd5
Tharki-God <alokender018@gmail.com>
2023-06-30 05:00:39 +0530

    better plaintext patch

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -6,7 +6,7 @@ export default [
     replacements: [
       {
         match: /this\.renderNameZone\(\),\(0,.\.\w+\)\([\w_$]+.\w+,{grow:0,children:\[/,
-        replace: `$&typeof replugged.plugins.getExports("dev.tharki.ReJoinVC")?.addPanelButton=="function"?replugged.plugins.getExports("dev.tharki.ReJoinVC")?.addPanelButton?.():null,`,
+        replace: `$&replugged.plugins.getExports("dev.tharki.ReJoinVC")?.addPanelButton?.()??null,`,
       },
     ],
   },
